df5c0f914cbfe0b989a8476678701654d08483a2
Rafael-Kendy <116125745+Rafael-Kendy@users.noreply.github.com>
2024-11-28 08:01:35 -0300

    mensagens de erro v3

--- Sintatico/Sintatico.java
+++ Sintatico/Sintatico.java
@@ -8,28 +8,10 @@ public class Sintatico {
 	TipoToken tt;
 	String erro_pcdec="esperado <PCDec, 'DEC'>";
 	String erro_pcprog="esperado <PCProg, 'PROG'>";
-	String erro_pcler="esperado <PCLer, 'LER'>";
-	String erro_pcimprimir="esperado <PCImprimir, 'IMPRIMIR'>";
-	String erro_pcse="esperado <PCSe, 'SE'>";
-	String erro_pcentao="esperado <PCEntao, 'ENTAO'>";
-	String erro_pcsenao="esperado <PCSenao, 'SENAO'>";
-	String erro_pcenqto="esperado <PCEnqto, 'ENQTO'>";
-	String erro_pcini="esperado <PCIni, 'INI'>";
-	String erro_pcfim="esperado <PCFim, 'FIM'>";
 	String erro_delim="esperado <Delim, ':'>";
-	String erro_atrib="esperado <Atrib, ':='>";
 	String erro_abrepar="esperado <AbrePar, '('>";
 	String erro_fechapar="esperado <FechaPar, ')'>";
-	String erro_var="esperado <Var, 'VARIAVEL'>";
-	
-	String erro_tipovar="esperado <TipoVar -> 'INT' | 'REAL'>";
-	String erro_comsaida2="esperado <ComSaida2 -> 'VARIAVEL' | 'CADEIA'>";
-	String erro_exparit2="esperado <ExpArit2 → '+ | -' ExpArit | e>";
-	String erro_termoarit2="esperado <TermoArit2 → '* | /' FatorArit TermoArit2 | e>";
-	String erro_fatorarit="esperado <FatorArit → 'NUMINT' | 'NUMREAL' | 'VARIAVEL' | '(' ExpArit ')'>";
-	String erro_exprel2="esperado <ExpRel2 → 'E | OU' TermoRel ExpRel2 | e;>";
-	String erro_termorel="esperado <TermoRel → ExpArit '< | > | <= | >= | == | !=' ExpArit | '(' ExpRel ')';>";
-	
+
 	public Sintatico(ArrayList<Token> tokens) {
 		this.tokens = tokens;
 		this.pos=0;
@@ -41,8 +23,11 @@ public class Sintatico {
 		if(tokens.get(pos).getTipo()==tipo) {
 			System.out.println("\t"+tokens.get(pos));
 			pos++;
-		}else {
+		}else if(erro!=null){
 			System.out.println("\tERRO Sintático na linha " + tokens.get(pos).getLinha() + ": " + erro);
+			System.exit(1);
+		}else {
+			System.out.println("\tERRO Sintático na linha " + tokens.get(pos).getLinha() + ": inesperado <" + tokens.get(pos).getTipo() + ", " + tokens.get(pos).getLexema() + ">");
 			System.exit(1);
 		}//if else erro
 	}//match
@@ -71,9 +56,9 @@ public class Sintatico {
 	
 	public void dec() { //Dec -> VARIAVEL ‘:’ TipoVar
 		tt=TipoToken.Var; //'VARIAVEL'
-		match(tt, erro_var);
+		match(tt, null);
 		tt=TipoToken.Delim; //':'
-		match(tt, erro_delim);
+		match(tt, null);
 		tipoVar();
 	}//listaDec2
 	
@@ -84,7 +69,7 @@ public class Sintatico {
 		}else {
 			tt=TipoToken.PCReal; //'REAL'
 		}//if int ou real
-		match(tt, erro_tipovar);
+		match(tt, null);
 	}//tipoVar
 	
 	
@@ -137,24 +122,24 @@ public class Sintatico {
 	
 	public void comAtri() { //ComAtri -> VARIAVEL ‘:=’ ExpArit
 		tt=TipoToken.Var; //'VARIAVEL'
-		match(tt, erro_var);
+		match(tt, null);
 		tt=TipoToken.Atrib; //':='
-		match(tt, erro_atrib);
+		match(tt, null);
 		expArit();
 	}//comAtri
 	
 	
 	public void comEnt() { //ComEnt -> ‘LER’ VARIAVEL;
 		tt=TipoToken.PCLer; //'LER'
-		match(tt, erro_pcler);
+		match(tt, null);
 		tt=TipoToken.Var; //'VARIAVEL'
-		match(tt, erro_var);
+		match(tt, null);
 	}//comEnt
 	
 	
 	public void comSaida() { //ComSaida -> ‘IMPRIMIR’ ComSaida2;
 		tt=TipoToken.PCImprimir; //'IMPRIMIR'
-		match(tt, erro_pcimprimir);
+		match(tt, null);
 		comSaida2();
 	}//comSaida
 	
@@ -165,17 +150,17 @@ public class Sintatico {
 		}else {
 			tt=TipoToken.Cadeia; //'CADEIA'
 		}//if var ou cadeia
-		match(tt, erro_comsaida2);
+		match(tt, null);
 	}//comSaida
 	
 	//////////////////////////////////////////////////////
 	
 	public void comCondicao() {//ComandoCondicao → 'SE' ExpressaoRelacional 'ENTAO' Comando ComandoCondicao2;
 	    tt = TipoToken.PCSe; //'SE'
-	    match(tt, erro_pcse);
+	    match(tt, null);
 	    expRel(); 
 	    tt = TipoToken.PCEntao; //'ENTAO'
-	    match(tt, erro_pcentao);
+	    match(tt, null);
 	    com(); 
 	    comCondicao2(); 
 	}//comCondicao
@@ -183,7 +168,7 @@ public class Sintatico {
 	public void comCondicao2() { //ComandoCondicao2 → Comando 'SENAO' | e;
 	    if (tokens.get(pos).getTipo() == TipoToken.PCSenao) {
 	        tt = TipoToken.PCSenao; //'SENAO'
-	        match(tt, erro_pcsenao);
+	        match(tt, null);
 	        com(); 
 	    } //se não houver 'SENAO', aceita vazio
 	}//comCondicao2
@@ -191,7 +176,7 @@ public class Sintatico {
 	
 	public void comRep() { //ComandoRepeticao → 'ENQTO' ExpressaoRelacional Comando;
 	    tt = TipoToken.PCEnqto; //'ENQTO'
-	    match(tt, erro_pcenqto);
+	    match(tt, null);
 	    expRel(); 
 	    com(); 
 	}//comRep
@@ -199,10 +184,10 @@ public class Sintatico {
 	
 	public void subAlg() { //SubAlgoritmo → 'INI' ListaComandos 'FIM';
 	    tt = TipoToken.PCIni; //'INI'
-	    match(tt, erro_pcini);
+	    match(tt, null);
 	    listaCom(); 
 	    tt = TipoToken.PCFim; //'FIM'
-	    match(tt, erro_pcfim);
+	    match(tt, null);
 	}//subAlg
 	
 	
@@ -215,7 +200,7 @@ public class Sintatico {
 	public void expArit2() { //ExpressaoAritmetica2 → '+' ExpressaoAritmetica | '-' ExpressaoAritmetica | e;
 	    if (tokens.get(pos).getTipo() == TipoToken.OpAritSoma || tokens.get(pos).getTipo() == TipoToken.OpAritSub) {
 	        tt = tokens.get(pos).getTipo(); //'+' ou '-'
-	        match(tt, erro_exparit2);
+	        match(tt, null);
 	        expArit();
 	    }
 	}//expArit2
@@ -230,7 +215,7 @@ public class Sintatico {
 	public void termoArit2() { //TermoAritmetico2 → '*' FatorAritmetico TermoAritmetico2 | '/' FatorAritmetico TermoAritmetico2 | e;
 	    if (tokens.get(pos).getTipo() == TipoToken.OpAritMult || tokens.get(pos).getTipo() == TipoToken.OpAritDiv) {
 	        tt = tokens.get(pos).getTipo(); //'*' ou '/'
-	        match(tt, erro_termoarit2);
+	        match(tt, null);
 	        fatorArit(); 
 	        termoArit2();
 	    } 
@@ -240,7 +225,7 @@ public class Sintatico {
 	public void fatorArit() { //FatorAritmetico → NUMINT | NUMREAL | VARIAVEL | '(' ExpressaoAritmetica ')'
 	    if (tokens.get(pos).getTipo() == TipoToken.NumInt || tokens.get(pos).getTipo() == TipoToken.NumReal || tokens.get(pos).getTipo() == TipoToken.Var) {
 	        tt = tokens.get(pos).getTipo(); //NUMINT, NUMREAL ou VARIAVEL
-	        match(tt, erro_fatorarit);
+	        match(tt, null);
 	    } else if (tokens.get(pos).getTipo() == TipoToken.AbrePar) {
 	        tt = TipoToken.AbrePar; //'('
 	        match(tt, erro_abrepar);
@@ -260,7 +245,7 @@ public class Sintatico {
 	public void expRel2() { //ExpressaoRelacional2 → OperadorBooleano TermoRelacional ExpressaoRelacional2 | e;
 	    if (tokens.get(pos).getTipo() == TipoToken.OpBoolE || tokens.get(pos).getTipo() == TipoToken.OpBoolOu) {
 	        tt = tokens.get(pos).getTipo(); //'E' ou 'OU'
-	        match(tt, erro_exprel2);
+	        match(tt, null);
 	        termoRel(); 
 	        expRel2(); 
 	    } 
@@ -282,7 +267,7 @@ public class Sintatico {
             tokens.get(pos).getTipo() == TipoToken.OpRelIgual ||
             tokens.get(pos).getTipo() == TipoToken.OpRelDif) {
             tt = tokens.get(pos).getTipo(); //pega o operador relacional
-            match(tt, erro_termorel);
+            match(tt, null);
         } 
         expArit(); 
     }
